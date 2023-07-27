6d1889fe8a5d493bec7f2d29d8891092ec9bae7a
liam <canoflime@gmail.com>
2023-07-27 13:35:24 +1200

    iterative polish
    added arcs and parameterised the gem pos, temp coloring, moving toward mandlebrot design

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,13 +2,18 @@
 let rect_width  = 20;
 let rect_height = 20;
 
-let corner_size = 20;
+let corner_size = 50;
+let gem_x = 0; //gem can be positioned relative to the centre, offset is determined below in the draw function
+let gem_y = 0;
 let centre_gem = true;
 let centre_gem_size = 100;
+let offsetx = 30;
+let offsety = -30;
+let colorpal=['rgb(255,128,255)','rgb(128,255,255)','rgb(255,255,128)'];
 let sw = 3;//stroke width
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(GRID_WALLPAPER); //develop_glyph
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -24,49 +29,33 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   rect(0,0,200,200);
+  fill(random(colorpal));
   stroke(sw);
-  //going to replace with arc() for a more artisnal aesthetic.
-  beginShape(LINES);
-    vertex(corner_size,0);
-    vertex(corner_size,corner_size/2);
-    vertex(corner_size,corner_size/2);
-    vertex(corner_size/2,corner_size/2);
-    vertex(corner_size/2,corner_size/2);
-    vertex(corner_size/2,corner_size);
-    vertex(corner_size/2,corner_size);
-    vertex(0,corner_size);
+  //arc(offsetx,offsety,200,200,0,360,OPEN);
+  noStroke();
+  rect(offsetx-corner_size/2,offsety-corner_size/2,corner_size+2,corner_size+2)
+  stroke(sw);
+  arc(offsetx+corner_size/2,offsety,corner_size,corner_size,-90,90);
+  arc(offsetx,corner_size/2+offsety,corner_size,corner_size,0,180);
+  arc(offsetx,offsety-corner_size/2,corner_size,corner_size,180,0);
+  arc(offsetx-corner_size/2,offsety,corner_size,corner_size,90,270);
+  
+  //arc(offsetx+90-corner_size/2, offsety+corner_size/2,corner_size, corner_size, 270, 0);
 
-    vertex(200-corner_size,200);
-    vertex(200-corner_size,200-corner_size/2);
-    vertex(200-corner_size,200-corner_size/2);
-    vertex(200-corner_size/2,200-corner_size/2);
-    vertex(200-corner_size/2,200-corner_size/2);
-    vertex(200-corner_size/2,200-corner_size);
-    vertex(200-corner_size/2,200-corner_size);
-    vertex(200,200-corner_size);
+  fill(random(colorpal));
 
-    vertex(200-corner_size,0);
-    vertex(200-corner_size,corner_size/2);
-    vertex(200-corner_size,corner_size/2);
-    vertex(200-corner_size/2,corner_size/2);
-    vertex(200-corner_size/2,corner_size/2);
-    vertex(200-corner_size/2,corner_size);
-    vertex(200-corner_size/2,corner_size);
-    vertex(200,corner_size);
+  stroke(1);
+  //rect(offsetx-10,offsety-10,20,20);
 
-    vertex(corner_size,200);
-    vertex(corner_size,200-corner_size/2);
-    vertex(corner_size,200-corner_size/2);
-    vertex(corner_size/2,200-corner_size/2);
-    vertex(corner_size/2,200-corner_size/2);
-    vertex(corner_size/2,200-corner_size);
-    vertex(corner_size/2,200-corner_size);
-    vertex(0,200-corner_size);
-  endShape();
-
+  fill(random(colorpal));
   if(centre_gem == true){
     stroke(sw);
-    ellipse(100,100,centre_gem_size,centre_gem_size);
+    ellipse(100+offsetx,100+offsety,centre_gem_size,centre_gem_size);
+    /*beginShape(LINES);
+      vertex(100+offsetx-35,100+offsety-35);
+      vertex(100+offsetx+35,100+offsety+35);
+    endShape();*/
+
   }
   
 }
