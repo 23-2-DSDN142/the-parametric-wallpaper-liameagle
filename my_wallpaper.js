2d43b9b03b9209e0c850bd355165558bd47ed689
liam <canoflime@gmail.com>
2023-08-03 11:49:16 +1200

    fixed offsets
    most elements should be individually offset now and using correct relative positioning, origins are also centered per object now aswell

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,20 +2,24 @@
 let rect_width  = 20;
 let rect_height = 20;
 
-let corner_size = 23;
+let corner_size = 35;
 let gem_x = 0; //gem can be positioned relative to the centre, offset is determined below in the draw function
 let gem_y = 0;
 let centre_gem = true;
-let centre_gem_size = 5;
-let offsetx = 100;
+let centre_gem_size = 20;
+let centre_gem_rot=167;
+let offsetx = 50;
 let offsety = 100;
-let colorpal=['#A5ECFA','#FAF7E6','#FAF5CD','#FACCA5',"#ECBEFA"];
-let sw = 3;//stroke width
+let gemoffsetx = 120;
+let gemoffsety = 130;
+let colorpal=['#FAF5CD','#FACCA5',"#ECBEFA"];
+let sw = 1;//stroke color
+let rotating=67;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH); //develop_glyph
+  pWallpaper.output_mode(GRID_WALLPAPER); //develop_glyph
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -35,24 +39,20 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   noStroke();
   rect(offsetx-corner_size/2,offsety-corner_size/2,corner_size+2,corner_size+2)
   stroke(sw);
-  fill(random(colorpal));
-  arc(offsetx+corner_size/2,offsety,corner_size,corner_size,-90,90);
-  arc(offsetx,corner_size/2+offsety,corner_size,corner_size,0,180);
-  arc(offsetx,offsety-corner_size/2,corner_size,corner_size,180,0);
-  arc(offsetx-corner_size/2,offsety,corner_size,corner_size,90,270);
-
   noStroke();
   //arc(offsetx+90-corner_size/2, offsety+corner_size/2,corner_size, corner_size, 270, 0);
   //arc(offsetx+90, offsety+corner_size*1.5,corner_size*1.3,corner_size*1.8, 180, 270);
+  //cross hatch
   fill(random(colorpal));
-  //accent lines
   rect(0,35,200,20);
   rect(35,0,20,200);
   rect(145,0,20,200);
   rect(0,145,200,20);
-  stroke(sw);
+  stroke(255);
+  fill(random(colorpal));
   rect(55,55,90,90);
   //side outlines
+  
   rect(0,55,200-(20*2)-35-90,90);
   rect(165,55,35,90);
   rect(55,0,90,35);
@@ -62,32 +62,41 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   rect(165,0,35,35);
   rect(165,165,35,35);
   rect(0,165,35,35);
-
   stroke(sw);
-  noFill()
-  line(15,75,75,15);
-  line(185,125,125,185);
-  line(15,125,75,185);
-  line(125,15,185,75);
-  arc(15,100,30,50,-90,-270);
-  arc(185,100,30,50,90,270);
-  arc(100,15,50,30,0,180);
-  arc(100,185,50,30,180,0);
-
-  line(100,0,100,200);
-  line(0,100,200,100);
-  stroke(1);
-  //rect(offsetx-10,offsety-10,20,20);
-
+  //centre shape diagonal thing with cutouts
   noFill();
-  arc(offsetx+corner_size/2,offsety,corner_size,corner_size,-90,90);
-  arc(offsetx,corner_size/2+offsety,corner_size,corner_size,0,180);
-  arc(offsetx,offsety-corner_size/2,corner_size,corner_size,180,0);
-  arc(offsetx-corner_size/2,offsety,corner_size,corner_size,90,270);
+  translate(offsetx,offsety);
+  rotate(rotating);
+  line(-85,-25,-25,-85);//nw
+  line(85,25,25,85);//se
+  line(-85,25,-25,85);//sw
+  line(25,-85,85,-25);//ne
+  arc(-85,0,30,50,-90,-270);
+  arc(85,0,30,50,90,270);
+  arc(0,-85,50,30,0,180);
+  arc(0,85,50,30,180,0);
+  rotate(-rotating);
+  translate(-offsetx,-offsety);
+  translate(100,100);
+  line(0,-100,0,100);
+  line(-100,0,100,0);
+  translate(-100,-100);
+  
+
+  translate(gemoffsetx,gemoffsety);
+  fill(random(colorpal));
+  rotate(centre_gem_rot);
+  noStroke();
+  rect(-corner_size/2,-corner_size/2,corner_size,corner_size);
+  stroke(sw);
+  arc(0+corner_size/2,0,corner_size,corner_size,-90,90);
+  arc(0,corner_size/2+0,corner_size,corner_size,0,180);
+  arc(0,0-corner_size/2,corner_size,corner_size,180,0);
+  arc(0-corner_size/2,0,corner_size,corner_size,90,270);
   if(centre_gem == true){
     fill(random(colorpal));
     stroke(sw);
-    ellipse(offsetx,offsety,centre_gem_size,centre_gem_size);
+    ellipse(0,0,centre_gem_size,centre_gem_size);
     /*beginShape(LINES);
       vertex(100+offsetx-35,100+offsety-35);
       vertex(100+offsetx+35,100+offsety+35);
